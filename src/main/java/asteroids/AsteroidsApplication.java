6fa7dd0612e597f9a36ad63b96b6325d73dd52d1
Ahmad Hamzah <hamzaha@mcmaster.ca>
2025-02-15 12:31:37 -0500

    ship movement

--- src/main/java/asteroids/AsteroidsApplication.java
+++ src/main/java/asteroids/AsteroidsApplication.java
@@ -1,14 +1,69 @@
 package asteroids;
 
-public class AsteroidsApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import java.util.Map;
+import java.util.HashMap;
+import javafx.animation.AnimationTimer;
+import javafx.scene.input.KeyCode;
+
+
+public class AsteroidsApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane pane = new Pane();
+        pane.setPrefSize(600, 400);
+
+        Ship ship = new Ship(150,100);
+
+        pane.getChildren().add(ship.getCharacter());
+        
+        Scene scene = new Scene(pane);
+        stage.setTitle("Asteroids!");
+        stage.setScene(scene);
+        
+        Map<KeyCode, Boolean> pressedKeys = new HashMap<>();
+
+        scene.setOnKeyPressed(event -> {
+            pressedKeys.put(event.getCode(), Boolean.TRUE);
+        });
+
+        scene.setOnKeyReleased(event -> {
+            pressedKeys.put(event.getCode(), Boolean.FALSE);
+        });
+
+        new AnimationTimer() {
+
+            @Override
+            public void handle(long now) {
+                if(pressedKeys.getOrDefault(KeyCode.LEFT, false)) {
+                    ship.turnLeft();
+                }
+
+                if(pressedKeys.getOrDefault(KeyCode.RIGHT, false)) {
+                    ship.turnRight();
+                }
+
+                if(pressedKeys.getOrDefault(KeyCode.UP, false)) {
+                    ship.accelerate();
+                }
+                
+                if(pressedKeys.getOrDefault(KeyCode.DOWN, false)) {
+                    ship.decelerate();
+                }
+                
+                ship.move();
+
+            }
+        }.start();
+        
+        stage.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        launch(args);
     }
-
-    public static int partsCompleted() {
-        // State how many parts you have completed using the return value of this method
-        return 0;
-    }
-
 }
